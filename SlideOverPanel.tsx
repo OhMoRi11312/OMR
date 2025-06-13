d9e66475fcb1cc979be8cfb65a72e316a18a1abe
Z:inO <jinhochoi19@gmail.com>
2025-06-13 09:31:50 +0900

    Update SlideOverPanel.tsx

--- SlideOverPanel.tsx
+++ SlideOverPanel.tsx
@@ -10,18 +10,20 @@ type Props = {
     drawerType?: any;
     backTouchable?: boolean;
     drawerStyle?: any;
+    drawerPosition?: any;
 }
-export default function SlideOver({ open, setOpen, innerComponent = <Text>slide over content</Text>, mainContent = <Text>main content</Text>, drawerType = 'front', backTouchable = true, drawerStyle }: Props) {
+export default function SlideOver({ open, setOpen, innerComponent = <Text>slide over content</Text>, mainContent = <Text>main content</Text>, drawerType = 'front', backTouchable = true, drawerStyle, drawerPosition = 'left' }: Props) {
 
     return (
         <Drawer
             open={open}
+            drawerPosition={drawerPosition}
             onOpen={() => setOpen(true)}
             onClose={() => setOpen(false)}
             renderDrawerContent={() => innerComponent}
             drawerType={drawerType}
             overlayStyle={(backTouchable ? {} : { zIndex: -1 })}
-            drawerStyle={[drawerStyle, (backTouchable ? {} : { borderColor: '#cccccc', borderRightWidth: 1, borderTopRightRadius: 25, borderBottomRightRadius: 25, })]}
+            drawerStyle={[drawerStyle, (backTouchable ? {} : { borderColor: '#cccccc', })]}
         >
             {mainContent}
         </Drawer>
